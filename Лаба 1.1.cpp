724721473858167e821ae70281dc8d19fa553767
JIAMA4455 <120753608+JIAMA4455@users.noreply.github.com>
2022-12-17 02:45:52 +0300

    continue

---
+++ Лаба 1.1.cpp
@@ -0,0 +1,16 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double U, R, I;
+	cout << "Введите значение напряжения (вольт) : ";
+	cin >> U;
+	cout << "Введите значение сопротивления (Ом) : ";
+	cin >> R;
+	cout << "Сила тока (ампер) : " << U / R;
+	return 0;
+}
