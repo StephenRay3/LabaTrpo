d565a34f016519ff478ad46d1f056a9a0c4a41f4
JIAMA4455 <120753608+JIAMA4455@users.noreply.github.com>
2022-12-24 00:51:47 +0300

    fsa

---
+++ лаба 1.5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int A, B;
+	cout << "A:";
+	cin >> A;
+	cout << "B:";
+	cin >> B;
+	if (A % 2 == 0 && B % 2 == 0 || A % 2 != 0 && B % 2 != 0) {
+		cout << "Истинно";
+	}
+	else
+	{
+		cout << "Ложь";
+	}
+}
