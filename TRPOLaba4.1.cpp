dc8b8838ab18aa7adfb09c141be00d7adaaba096
JluSTEP <120753651+JluSTEP@users.noreply.github.com>
2022-12-24 01:34:21 +0300

    Finishing project

--- TRPOLaba4.1.cpp
+++ TRPOLaba4.1.cpp
@@ -277,3 +277,332 @@ void Ex5(string text)
 
     system("pause");
 }
+
+vector<int> searchNumbers(string text)
+{
+    vector<string> words = SplitToWords(text);
+    vector<int> numbers = {};
+
+    for (int i = 0; i < words.size(); i++)
+    {
+        if (words[i][0] >= 48 && words[i][0] <= 57) {
+            numbers.push_back(atoi(words[i].c_str()));
+        }
+    }
+
+    return numbers;
+}
+
+vector<string> SplitToEachSentence(string text)
+{
+    vector<string> sentencies = {};
+    string sentence, temp_sentence;
+
+    for (int i = 0; i < text.length(); i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?') {
+            sentence += text[i];
+            sentencies.push_back(sentence);
+            sentence = "";
+            if (text[i + 1] == ' ') {
+                i++;
+            }
+        }
+        else if (i == text.length() - 1) {
+            sentence += text[i];
+            sentencies.push_back(sentence);
+        }
+        else {
+            sentence += text[i];
+        }
+    }
+
+    return sentencies;
+}
+
+vector<string> SplitToWords(string text)
+{
+    vector<string> words = {};
+    string word;
+
+    for (int i = 0; i < text.length(); i++)
+    {
+        if (i == text.length() - 1 && text[i] == '.' || i == text.length() - 1 && text[i] == ' ' || i == text.length() - 1 && text[i] == '!' || i == text.length() - 1 && text[i] == '?') {
+            words.push_back(word);
+        }
+        else if (text[i] == ' ') {
+            words.push_back(word);
+            word = "";
+        }
+        else if (text[i] == '!' || text[i] == '.' || text[i] == '?' || text[i] == ',') {
+            continue;
+        }
+        else {
+            word += text[i];
+        }
+    }
+
+    return words;
+}
+
+vector<string> MinWordsArray(string text)
+{
+    int minLength = SearchTheMinWord(text);
+
+    char del[] = "0123456789";
+
+    for (unsigned i = 0; i < strlen(del); i++)
+    {
+        text.erase(std::remove(text.begin(), text.end(), del[i]), text.end());
+    }
+
+    string temp;
+
+    for (unsigned i = 0; i < text.length(); i++)
+    {
+        if (text[i] == ' ' && text[i + 1] == ' ' && i + 1 < text.length()) {
+            temp += ' ';
+            i++;
+        }
+        else if (i == text.length() - 1 && text[i] == ' ') {
+            // ПРОСТО ПРОИГНОРИРУЕМ ПОСЛЕДНИЙ ПРОБЕЛ, ПШЕЛ ОН НАХЕР!
+        }
+        else {
+            temp += text[i];
+        }
+    }
+
+    vector<string> minWords = {};
+    vector<string> words = SplitToWords(temp);
+
+    for (int i = 0; i < words.size(); i++)
+    {
+        if (words[i].length() == minLength)
+        {
+            minWords.push_back(words[i]);
+        }
+    }
+
+    return minWords;
+}
+
+string FindPalindroms(string text)
+{
+    vector<string> words = SplitToWords(text);
+    string longestPalindrom = "", tempPalindrom;
+    bool flag = true;
+
+    for (int i = 0; i < words.size(); i++)
+    {
+        for (int j = 0, k = words[i].length() - 1; j < words[i].length() / 2; j++, k--)
+        {
+            if (words[i][j] != words[i][k]) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag) {
+            tempPalindrom = words[i];
+        }
+
+        if (longestPalindrom.length() < tempPalindrom.length()) {
+            longestPalindrom = tempPalindrom;
+        }
+
+        flag = true;
+    }
+
+    if (longestPalindrom == "") {
+        return "здесь нет палиндромов";
+    }
+    else {
+        return longestPalindrom;
+    }
+}
+
+string toBinary(int value)
+{
+    string binary;
+
+    while (value != 0) {
+        binary += (value % 2 == 0 ? "0" : "1");
+        value /= 2;
+    }
+
+    reverse(binary.begin(), binary.end());
+
+    return binary;
+}
+
+string returnReverseString(string str)
+{
+    int length = str.length();
+    string reversed = str;
+
+    for (int i = 0; i < length / 2; i++)
+        swap(reversed[i], reversed[length - i - 1]);
+
+    return reversed;
+}
+
+int SearchTheMinWord(string text)
+{
+    char del[] = "0123456789";
+
+    for (unsigned i = 0; i < strlen(del); i++)
+    {
+        text.erase(std::remove(text.begin(), text.end(), del[i]), text.end());
+    }
+
+    string temp;
+
+    for (unsigned i = 0; i < text.length(); i++)
+    {
+        if (text[i] == ' ' && text[i + 1] == ' ' && i + 1 < text.length()) {
+            temp += ' ';
+            i++;
+        }
+        else if (i == text.length() - 1 && text[i] == ' ') {
+            // ПРОСТО ПРОИГНОРИРУЕМ ПОСЛЕДНИЙ ПРОБЕЛ, ПШЕЛ ОН НАХЕР!
+        }
+        else {
+            temp += text[i];
+        }
+    }
+
+    vector<string> words = SplitToWords(temp);
+    int minLength = INT_MAX;
+
+    for (int i = 1; i < words.size(); i++)
+    {
+        if (words[i].length() < minLength)
+        {
+            minLength = words[i].length();
+        }
+    }
+
+    return minLength;
+}
+
+int romanToInt(string text)
+{
+    //ассоциативный контейнер, который хранит по принципу (ключ - значение) и при добавлении нового элемента отсортировывает себя
+    map<char, int> romanValues = { { 'I',1 }, { 'V',5 },{ 'X',10 },{ 'L',50 },{ 'C',100 },{ 'D',500 },{ 'M',1000 } };
+
+    int limit = text.size();
+    int retVal = 0;
+    for (int i = 0; i < limit; i++)
+    {
+        auto valueIter = romanValues.find(text[i]);
+        if (valueIter != romanValues.end())
+        {
+            int modifier = 1;
+            if (((i + 1) < limit) && (valueIter->second < romanValues[text[i + 1]]))
+            {
+                modifier = -1;
+            }
+            retVal += valueIter->second * modifier;
+        }
+    }
+    return retVal;
+}
+
+int searchRomanNumbers(string t)
+{
+    string r[13] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+    int b, i; // индекс текущего символа в тексте и индекс текущей римской цифры в массиве r
+
+    for (b = 0, i = 0; b < t.length() && i < 13; ++i)
+    {
+        int rl = r[i].length(); // количество символов в элементе массива
+        if (t.substr(b, rl) == r[i])
+        {
+            if (rl == 1 && i != 0)
+            {
+                int count = 1; // количество повторов
+                while (++b < t.length() && t.substr(b, rl) == r[i]) ++count; // пропуск и подсчёт повторяющихся цифр
+                if (count > 3)
+                {
+                    --b;
+                    break;
+                }
+            }
+            else b += rl;
+        }
+    }
+
+    if (b < t.length()) {
+        //Не римское число
+        return -1;
+    }
+    else {
+        //Римское число
+        return romanToInt(t);
+    }
+}
+
+int toDecimal(int number)
+{
+    int decimal = 0;
+    int rem = 0;
+    int i = 0;
+
+    while (number != 0) {
+        rem = number % 10;
+        number /= 10;
+        decimal += rem * pow(2, i);
+        ++i;
+    }
+
+    return decimal;
+}
+
+void searсhQuote(string t)
+{
+    // Lorem abcd ada ipsum "afgfa dolor 8 sit 187 amet". 
+    // Consectetur abcdcba 13 adipiscing aba 34 elit. 
+    // "Sed do eiusmod tempor incididunt ut 87 labore et VI dolore XIV magna aliqua. Lorem ipsum VI dolor XIV sit amet\".
+
+    int counter = 0;
+    bool isQuote = false;
+
+    for (char ch : t)
+    {
+        if (ch == '\"')
+            counter++;
+    }
+
+    if (counter % 2 != 0) {
+        cout << "Error: Odd number of quotes! (" << counter << ")" << endl;
+        system("pause");
+        return;
+    }
+
+    vector<string> quotes = {};
+    string temp;
+
+    for (char ch : t) {
+        if (ch == '\"' && !isQuote) {
+            isQuote = true;
+        }
+        else if (ch == '\"' && isQuote) {
+            isQuote = false;
+            quotes.push_back(temp);
+            temp = "";
+        }
+
+        if (ch != '\"' && isQuote) {
+            temp += ch;
+        }
+    }
+
+    cout << "Counter: " << counter << endl;
+    cout << "Quotes:" << endl;
+
+    for (string quote : quotes) {
+        cout << quote << endl;
+    }
+
+    system("pause");
+}
